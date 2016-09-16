56b2ed9be13102d3dc7acda55c6b5c1e843b07cd
lusm <135719728@qq.com>
2016-09-16 17:06:31 +0800

    安装linux软件脚本

---
+++ installSortware.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+sudo add-apt-repository ppa:webupd8team/java
+sudo apt-get update
+sudo apt-get install oracle-java7-installer 
+
+sudo add-apt-repository ppa:webupd8team/brackets
+sudo apt-get update
+sudo apt-get install brackets 
+
+sudo apt-get install guake
+
+sudo apt-get install mysql-server-5.7
+
+sudo apt-get install git 
+
+sudo apt-get install fcitx
+
